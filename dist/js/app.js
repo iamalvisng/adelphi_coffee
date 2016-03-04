bc0bd58bec9606b2e50ca2bced8ab76210f19ecb
Alvis <viz.flux@gmail.com>
2016-03-04 14:46:58 +0800

    Changed URL path for JS files

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){smoothScroll.init({speed:1e3,easing:"easeInOutCubic",updateURL:!0}),$(".owl-carousel").owlCarousel({items:1})});
+$(document).ready(function(){smoothScroll.init({speed:1e3,easing:"easeInOutCubic",updateURL:!0});$(".owl-carousel").owlCarousel({items:1});});
